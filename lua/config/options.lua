038ae7a45870abf46f008afdda67ab75b1967771
UmutKzl <umutkzl@protonmail.com>
2026-06-07 00:32:33 +0300

    feat: add some options
    enable relative numbers
    change font on neovide
    set updatetime to 300ms

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Neovide settigns
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h14"
+end
+
+vim.opt.relativenumber = true
+
+vim.opt.updatetime = 300
